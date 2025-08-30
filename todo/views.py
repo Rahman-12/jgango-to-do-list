61441ce3d2cd2b3208378b743504fb1c7b412dc8
Rahman-12 <yusuf.dammy2019@gmail.com>
2025-08-30 16:57:02 +0100

    simple django todo list is being processed v imp

---
+++ todo/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from .models import Task
+
+
+def index(request):
+    tasks= Task.objects.all()
+    return render(request,"index.html",{"tasks":tasks})
