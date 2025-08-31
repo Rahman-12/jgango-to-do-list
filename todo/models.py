0f6e2b7a75af20fc841d72910cd5d58f50fce41c
Rahman-12 <yusuf.dammy2019@gmail.com>
2025-08-31 12:01:06 +0100

    not working

--- todo/models.py
+++ todo/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
     complete = models.BooleanField(default=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+                             on_delete=models.CASCADE,
+                             null=True)  # Allow null for existing tasks
+    # Default to now for existing tasks
+    created_at = models.DateTimeField(default=timezone.now)
+    # This will work without issues
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
